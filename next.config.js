f5f3a813b42947e82b62a2a258919e146c9a1db1
Tina Tarighian <tarighian@google.com>
2024-10-10 20:37:23 -0400

    Fix next.config.js and build script

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     unoptimized: true,
   },
   // If your repository name is different from your GitHub username, uncomment and adjust these:
-  // assetPrefix: '/your-repo-name/',
-  // basePath: '/your-repo-name',
+  assetPrefix: '/parsonslecture/',
+  basePath: '/parsonslecture',
 }
 
 module.exports = nextConfig
