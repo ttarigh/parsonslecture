e6974f08892f9d0a94e9bd352ea6093f8e77750f
ttarigh <ttarigh@users.noreply.github.com>
2024-10-11 00:48:16 +0000

    deploy: 4ce10b51d4850a4b1cadc45609fdce2884c0a89b

---
+++ _next/static/zPMMD184YkBgbMz6u2lDm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/87a5f2d1e660d3d7.css","static/chunks/pages/index-fd99cafbe27b3f2a.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
